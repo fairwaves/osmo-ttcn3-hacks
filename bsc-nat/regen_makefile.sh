77df5905d2912275a5ad03ac906ebfab19b29108
nikita.roshupkin <nikita.roshupkin@fairwaves.co>
2019-03-19 21:16:30 +0200

    [GSTA-feature] Creating a testing system.

--- bsc-nat/regen_makefile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-MAIN=IPA_Test.ttcn
-
-FILES="*.ttcn *.ttcnpp SCCP_EncDec.cc IPA_CodecPort_CtrlFunctDef.cc IPL4asp_PT.cc IPL4asp_discovery.cc TCCConversion.cc TCCInterface.cc RTP_EncDec.cc SDP_EncDec.cc *.c MGCP_CodecPort_CtrlFunctDef.cc"
-
-export CPPFLAGS_TTCN3="-DIPA_EMULATION_SCCP -DIPA_EMULATION_MGCP -DUSE_MTP3_DISTRIBUTOR"
-
-../regen-makefile.sh $MAIN $FILES
