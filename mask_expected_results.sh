93b261c6392f9fcaab20356a5aa9ff1c62d620fe
nikita.roshupkin <nikita.roshupkin@fairwaves.co>
2019-03-20 23:50:52 +0200

    [GSTA-feature] Creating a testing system v3.

--- mask_expected_results.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# to prevent diffs due to timing and source file lines, mask some numbers
-
-for target in */expected-results.xml; do
-  sed -i "s/time='[^']*'/time='MASKED'/g" "$target"
-  sed -i 's/ttcn:[0-9]\+/ttcn:MASKED/g' "$target"
-done
--- rmlinks.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-find . -type l -not -path "./bin/*" -exec rm \{\} \;
